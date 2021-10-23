4330ac7e15bcd78626173c2b57dcf3ee01ea5760
miguelangelmolinaforero <68454714+miguelangelmolinaforero@users.noreply.github.com>
2021-10-22 20:27:09 -0500

    Subida directorios
    Subida directorios de nuevo repositorio del RETO

---
+++ ejercicio_galeria/js/main.js
@@ -0,0 +1,38 @@
+// Contador
+var contadorImagen = 1;
+mostrarImagenes(contadorImagen);
+
+// Botones
+function sumarImagen(numero_imagen) {
+    mostrarImagenes(contadorImagen += numero_imagen);
+}
+
+// Mostrar imagen de la fila
+function imagenActual(numero_imagen) {
+    mostrarImagenes(contadorImagen = numero_imagen);
+}
+
+function mostrarImagenes(numero_imagen) {
+    var i;
+    var imagenes = document.getElementsByClassName("imagen");
+    var dots = document.getElementsByClassName("demo");
+
+    if (numero_imagen > imagenes.length) {
+        contadorImagen = 1
+    }
+
+    if (numero_imagen < 1) {
+        contadorImagen = imagenes.length
+    }
+
+    for (i = 0; i < imagenes.length; i++) {
+        imagenes[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" primer_plano", "");
+    }
+
+    imagenes[contadorImagen - 1].style.display = "block";
+    dots[contadorImagen - 1].className += " primer_plano";
+}
