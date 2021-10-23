fba501e070a7257dc228bc28649472f44a54f91b
miguelangelmolinaforero <68454714+miguelangelmolinaforero@users.noreply.github.com>
2021-10-22 21:14:51 -0500

    Cambios de estilos

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function mostrarImagenes(numero_imagen) {
     var i;
     var imagenes = document.getElementsByClassName("imagen");
     var dots = document.getElementsByClassName("demo");
+    var descripcion = document.getElementById("descripcion_imagen");
 
     if (numero_imagen > imagenes.length) {
         contadorImagen = 1
@@ -35,4 +36,5 @@ function mostrarImagenes(numero_imagen) {
 
     imagenes[contadorImagen - 1].style.display = "block";
     dots[contadorImagen - 1].className += " primer_plano";
+    descripcion.innerHTML = dots[contadorImagen - 1].alt;
 }
